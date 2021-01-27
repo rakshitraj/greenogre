907926e40c62e471a9bb90f8ce8e09778a80a6f2
rakshitraj <rraj.rakshit@gmail.com>
2021-01-27 22:13:00 +0530

    region of interest

--- finding-lanes/lanes.py
+++ finding-lanes/lanes.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 def canny(image):
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
@@ -7,13 +8,26 @@ def canny(image):
     edge = cv2.Canny(blur, 50, 150)
     return edge
 
+def region_of_interest(image):
+    height = image.shape[0]
+    triangle = np.array([(200, height), (1100, height), (500, 250)])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, triangle, 255)
+    return mask
+
+
 # Read the image
 image = cv2.imread('test_image.jpg')
-cpy_image = image
-cv2.imshow('image', image)
+cpy_image = np.copy(image)
+# cv2.imshow('image', image)
 
 # Canny edge detection
 canny = canny(cpy_image)
-cv2.imshow('Edges', canny)
+# cv2.imshow('Edges', canny)
 
-cv2.waitKey(0)
+# Region of interest  #
+# ------------------- #
+plt.imshow(canny)
+plt.show()
+
+# cv2.waitKey(0)
